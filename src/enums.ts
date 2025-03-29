fad49435af81258e514ddb57ff9d4dccad258f7c
Prog <progsolos@gmail.com>
2025-03-29 07:41:28 +0530

    tuples and enums

---
+++ src/enums.ts
@@ -0,0 +1,11 @@
+enum WeatherConditions {
+    Sunny = "sunny",
+    Cloudy = "cloudy",
+    Rainy = "rainy",
+    Snowy = "snowy",
+}
+
+console.log(WeatherConditions)
+
+const currentWeather = WeatherConditions.Snowy
+console.log(`The current weather is ${currentWeather}`)
---
+++ src/tuples.ts
@@ -0,0 +1,10 @@
+let myTuple: [string, number] = ["hello", 22]
+console.log(myTuple[0])
+console.log(myTuple[1])
+
+// destructuring individual element
+let [first, second] = myTuple
+console.log(first)
+console.log(second)
+
+const games: [string, number, string] = ["Game 1", 22, "Game 2"]
